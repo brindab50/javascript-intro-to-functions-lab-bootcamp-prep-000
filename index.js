657509fe5afdf5ef3ab4a221e284c57102946d3a
brindab50@gmail.com behal <brindab50@gmail.com>
2018-08-27 18:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma(string) {
-  return "YES INDEED!".toUpperCase()
+  if(string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+if (string ===string.toLowerCase()) {
+  return "I can't hear you!"
 }
+if (string === "I love you, Grandma!") {
+  return "I love you, too!"
+}
+ 
